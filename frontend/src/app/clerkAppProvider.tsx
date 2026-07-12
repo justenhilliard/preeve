b6de4e9f7f3e75c304566d0c3f1f533f99fd7034
justenhilliard <justenkhilliard@gmail.com>
2026-07-12 13:01:18 -0400

    Wrap app in TanStack QueryClientProvider

--- frontend/src/app/clerkAppProvider.tsx
+++ frontend/src/app/clerkAppProvider.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export function ClerkAppProvider({
   children,
@@ -9,13 +11,14 @@ export function ClerkAppProvider({
   children: React.ReactNode;
 }>) {
   const pathname = usePathname();
+  const [queryClient] = useState(() => new QueryClient());
   const formButtonPrimary = pathname.startsWith("/sign-in")
     ? "Log In"
     : "Create Account";
 
   return (
     <ClerkProvider localization={{ formButtonPrimary }}>
-      {children}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </ClerkProvider>
   );
 }
